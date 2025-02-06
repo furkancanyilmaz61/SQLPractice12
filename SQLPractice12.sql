57453dfe90ee834973c3d247f136411cacb696b4
Furkancan <furkancaanyilmaz@gmail.com>
2025-02-06 18:25:27 +0300

    Add files via upload

---
+++ SQLPractice12.sql
@@ -0,0 +1,18 @@
+SELECT COUNT(*)
+FROM film 
+WHERE length > (SELECT AVG (length)FROM film);
+
+SELECT COUNT(*)
+FROM film
+WHERE rental_rate = (SELECT MAX(rental_rate)FROM film);
+
+SELECT *
+FROM film
+WHERE rental_rate = (SELECT MIN(rental_rate)FROM film)
+AND replacement_cost = (SELECT MIN(replacement_cost)FROM film);
+
+SELECT customer.customer_id, customer.first_name, customer.last_name, COUNT(payment.payment_id) AS total_purchases
+FROM customer
+JOIN payment ON customer.customer_id = payment.customer_id
+GROUP BY customer.customer_id, customer.first_name, customer.last_name
+ORDER BY total_purchases DESC;
